b463a1a8d6a5716cc4772e6df8d8a5940d0e0ad3
Andrew Griesemer <ajgriesemer@gmail.com>
2017-04-24 22:35:06 -0500

    Added dispatch structure to xbee test application. Had to purge all python2 syntax.

--- xbee_test.py
+++ xbee_test.py
@@ -1,32 +1,66 @@
-import serial
-import time
+"""https://github.com/liwp/python-xbee-demo/blob/master/xbee-demo.py"""
+
 from xbee import XBee
-import xbee as xb
+from xbee.helpers.dispatch import Dispatch
+import serial
+import struct
 import time
+import os
 
-serial_port = serial.Serial('/dev/ttyS0', 9600)
+HUB_ADDR = struct.pack('>Q', 0)  # 64-bit 0s
+BCAST_ADDR = '\xFF\xFE'
 
-def print_data(data):
-    """
-    This method is called whenever data is received
-    from the associated XBee device. Its first and
-    only argument is the data contained within the
-    frame.
-    """
-    print(data)
+IS_HUB = bool(os.getenv('IS_HUB'))
 
-xbee = XBee(serial_port, callback=print_data,escaped=True)
+
+def default_handler(name, packet):
+    print("%s - %s" % (name, packet))
+
+
+def at_response_handler(name, packet):
+    print(packet)
+
+
+def rx_handler(name, packet):
+    print("RX - %s" % packet)
+    time.sleep(1)
+    data = "PONG" if IS_HUB else "PING"
+    xbee.tx(dest_addr_long=packet['source_addr_long'], dest_addr=packet['source_addr'], data=data)
+
+
+ser = serial.Serial('/dev/ttyS0', 9600)
+dispatch = Dispatch()
+dispatch.register('rx', rx_handler, lambda p: p['id'] == 'rx')
+dispatch.register('rx_explicit', default_handler, lambda p: p['id'] == 'rx_explicit')
+dispatch.register('rx_io_data_long_addr', default_handler, lambda p: p['id'] == 'rx_io_data_long_addr')
+dispatch.register('rx_io_data', default_handler, lambda p: p['id'] == 'rx_io_data')
+dispatch.register('tx_status', default_handler, lambda p: p['id'] == 'tx_status')
+dispatch.register('status', default_handler, lambda p: p['id'] == 'status')
+dispatch.register('at_response', at_response_handler, lambda p: p['id'] == 'at_response')
+dispatch.register('remote_at_response', default_handler, lambda p: p['id'] == 'remote_at_response')
+dispatch.register('node_id_indicator', default_handler, lambda p: p['id'] == 'node_id_indicator')
+
+xbee = XBee(ser, callback=dispatch.dispatch, escaped=True)
+dispatch.xbee = xbee
+
+print("run...")
+print("")
+print("current status:")
+xbee.send('at', command=b'ID', frame_id=b'\x01')
+xbee.send('at', command=b'SH', frame_id=b'\x01')
+xbee.send('at', command=b'SL', frame_id=b'\x01')
+xbee.send('at', command=b'MY', frame_id=b'\x01')
+print("")
+
 time.sleep(1)
-xbee.send("at", frame_id=b'\x01', command=b'MY')
-xbee.send("at", frame_id=b'\x01', command=b'SH')
-xbee.send("at", frame_id=b'\x01', command=b'SL')
-time.sleep(1)
 
+print("")
 while True:
     try:
-        time.sleep(0.001)
+        time.sleep(2)
     except KeyboardInterrupt:
         break
 
 xbee.halt()
-serial_port.close()
+
+ser.close()
