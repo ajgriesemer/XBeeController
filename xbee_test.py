a9d7077e96442565a09db48ab55df8e9578f3002
Andrew Griesemer <ajgriesemer@gmail.com>
2017-04-24 22:34:13 -0500

    Created simple test application to query a command from the XBee module. I got tripped up not realizing that if the frame_id is left at default there is no response packet.

--- xbee_test.py
+++ xbee_test.py
@@ -1,13 +1,32 @@
 import serial
+import time
 from xbee import XBee
+import xbee as xb
+import time
 
 serial_port = serial.Serial('/dev/ttyS0', 9600)
-xbee = XBee(serial_port)
+
+def print_data(data):
+    """
+    This method is called whenever data is received
+    from the associated XBee device. Its first and
+    only argument is the data contained within the
+    frame.
+    """
+    print(data)
+
+xbee = XBee(serial_port, callback=print_data,escaped=True)
+time.sleep(1)
+xbee.send("at", frame_id=b'\x01', command=b'MY')
+xbee.send("at", frame_id=b'\x01', command=b'SH')
+xbee.send("at", frame_id=b'\x01', command=b'SL')
+time.sleep(1)
 
 while True:
     try:
-        print(xbee.wait_read_frame())
+        time.sleep(0.001)
     except KeyboardInterrupt:
         break
 
+xbee.halt()
 serial_port.close()
