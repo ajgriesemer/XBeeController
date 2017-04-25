4ecad9a9c62e3e658ba89c332f741008f7bfa677
Andrew Griesemer <ajgriesemer@gmail.com>
2017-04-25 08:55:57 -0500

    Created quick application to demonstrate a tabbed layout.

--- gui_test.py
+++ gui_test.py
@@ -1,8 +1,25 @@
-from tkinter import *
-root = Tk()
-Label(root, text="Username").grid(row=0, sticky=W)
-Label(root, text="Password").grid(row=1, sticky=W)
-Entry(root).grid(row=0, column=1, sticky=E)
-Entry(root).grid(row=1, column=1, sticky=E)
-Button(root, text="Login").grid(row=2, column=1, sticky=E)
-root.mainloop()
+import tkinter
+from tkinter import ttk
+
+def main():
+    root = tkinter.Tk()
+    root.title("XBee Controller")
+    root.minsize(width=720, height=400)
+    notebook = ttk.Notebook(root)
+
+    # adding Frames as pages for the ttk.Notebook
+    # first page, which would get widgets gridded into it
+    page1 = tkinter.Frame(notebook)
+
+    # second page
+    page2 = tkinter.Frame(notebook)
+
+    notebook.add(page1, text='One')
+    notebook.add(page2, text='Two')
+
+    notebook.pack(expand=1, fill="both")
+
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
